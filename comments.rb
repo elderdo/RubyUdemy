78e19cd914aa8d0f7c81d4bf92adc6bc6f9dc620
elderdo <elderdo@yahoo.com>
2018-01-05 22:08:29 -0600

    First commit

---
+++ comments.rb
@@ -0,0 +1,10 @@
+puts "Hello world!"  # this is a comment
+=begin
+puts "Hello world!"  # this is a comment
+puts "Hello world!"  # this is a comment
+puts "Hello world!"  # this is a comment
+puts "Hello world!"  # this is a comment
+=end
+
+puts 'Hello Ruby \nwhat\'s up'
+puts "\t I\'m 6\' 4\"\n\n"
