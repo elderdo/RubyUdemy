e535fbcb80efec8f417dec7b3396470d17db8ecb
elderdo <elderdo@yahoo.com>
2018-01-07 13:59:16 -0600

    defined PEMDAS

---
+++ section-6/pemdas.rb
@@ -0,0 +1,12 @@
+# https://en.wikipedia.org/wiki/Order_of_operations
+# Please excuse my dear aunt sally
+PEMDAS =
+"
+P - Parentheses '()'
+E - Exponents '**'
+M - Multiplication '*'
+D - Division
+A - Addition
+S - Substraction
+"
+puts PEMDAS
