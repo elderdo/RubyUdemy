5d7a81e863e8b131fb8e4891ee94d7f3d4f5156b
elderdo <elderdo@yahoo.com>
2018-01-08 22:10:28 -0600

    section 9 complete

--- section_9/ruby_needs_a_nap.rb
+++ section_9/ruby_needs_a_nap.rb
@@ -4,11 +4,19 @@ def app_name
   first_name = gets.chomp.capitalize.strip
   if first_name == ""
     puts "You must enter a valid name"
+    puts "Rebooting application please wait."
+    sleep 1
+    puts "Please wait.."
+    sleep 1.3
+    puts "One moment.."
+    sleep 1.8
+    puts "Still working.."
+    sleep 2
+    puts "Almost done.."
+    sleep 1
     app_name
   else
     puts "Welcome to the console app #{first_name}"
-    p first_name.inspect
-    p first_name.size
     puts "Your first name contains #{first_name.size} characters"
   end
 end
